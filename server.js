053785402ff5b21dba78178465b55beb750c34b0
Antoine Chagnon-Michaud <antchmcg@cegeprdl.ca>
2024-11-05 15:37:15 -0500

    done

--- server.js
+++ server.js
@@ -2,37 +2,47 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3001;
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  if (!req.cookies.randomCookie) {
+    const userId = Math.random().toString(36).substring(2, 15);
+    res.cookie('userId', userId);
+  }
+  next();
+});
 
 app.use(cors());
 // Parse JSON request bodies
 app.use(express.json());
-global.message =  {
-  from: "System",
-  message: "Hi, this is the system",
-  date: new Date()
+const default_developper = {	
+  name: 'Antoine Chagnon Michaud',
+      age: 35,
+      isAProWithReact: true,
+      salary: 1000000,
 }
-// Create a route to log the request body
-app.get('/message', (req, res) => {
-  res.send(global.message);
+
+global.users =  {
+}
+      
+app.get('/developper', (req, res) => {
+  if (global.users[req.cookies.randomCookie])
+    return res.send(global.users[req.cookies.randomCookie]);
+  return res.send(default_developper)
 });
 
-app.post('/message', (req, res) => {
-  if (req.body.from && req.body.message) {
-    if (req.body.message === "anyone there?") {
-      global.message = { 
-        from: "System",
-        message: "Chu là!",
-        date: new Date()
-      }
-    }
-    else {
-      global.message = { 
-        from: req.body.from,
-        message: req.body.message,
-        date: new Date()
-      }
-    }
-  }
+app.post('/developper', (req, res) => {
+  console.log("🚀 ~ app.post ~ req.body:", req.body)
+  if (!req.body.name)
+    return res.status(400).send("Missing required name");
+  if (!req.body.age || req.body.age < 0)
+    return res.status(400).send("Missing or invalid required age");
+  if (req.body.isAProWithReact === undefined)
+    return res.status(400).send("Missing required isAProWithReact");
+  if (!req.body.salary || req.body.salary < 0)
+    return res.status(400).send("Missing or invalid required salary");
+  global.users[req.cookies.randomCookie] = req.body
   res.status(201).send("Message created");
 });
 
